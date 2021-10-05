c2296f639f43fe0e6878f706b0e291e26e1e46df
Benjamin Haegenlaeuer <benjamin.haegenlaeuer@sap.com>
2021-10-05 15:04:17 +0200

    Add status codes to responses
    Co-authored-by: Philipp Stehle <philipp.stehle@sap.com>

--- src/main/java/com/sap/cloud/service/flags/demo/controller/DemoController.java
+++ src/main/java/com/sap/cloud/service/flags/demo/controller/DemoController.java
@@ -2,12 +2,15 @@ package com.sap.cloud.service.flags.demo.controller;
 
 import java.util.Optional;
 
+import javax.servlet.http.HttpServletResponse;
+
+import com.sap.cloud.service.flags.demo.service.FeatureFlagsService;
+import com.sap.cloud.service.flags.demo.service.FlagStatus;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-
-import com.sap.cloud.service.flags.demo.service.FeatureFlagsService;
 
 /**
  * Represents a controller that uses the feature toggling practice.
@@ -52,17 +55,23 @@ public class DemoController {
 	 *            - ID of the feature flag
 	 * @param modelMap
 	 *            - the {@link ModelMap}
+	 * @param rsp
+	 *            - the {@link HttpServletResponse}
 	 * 
 	 * @return the evaluation view
 	 */
 
 	@GetMapping("/evaluate/{id}")
-	public String evaluate(@PathVariable final String id, final ModelMap modelMap) {
+	public String evaluate(@PathVariable final String id, final ModelMap modelMap, final HttpServletResponse rsp) {
 		String status;
 		if (!hasBoundServiceInstance()) {
 			status = "missing-service-instance";
+			rsp.setStatus(500);
 		} else {
 			status = featureFlagsServiceOptional.get().getFlagStatus(id).toString();
+			if (status.equals(FlagStatus.MISSING.toString())) {
+				rsp.setStatus(404);
+			}
 		}
 
 		modelMap.addAttribute("status", status);
