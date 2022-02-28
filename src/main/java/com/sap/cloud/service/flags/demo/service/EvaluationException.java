b879b32eddf00c3db34037a8de51a740bc6fc4b1
Petar Ivanov <20043863+petar-iv@users.noreply.github.com>
2022-02-28 17:27:56 +0200

    Use tabs instead of spaces

--- src/main/java/com/sap/cloud/service/flags/demo/service/EvaluationException.java
+++ src/main/java/com/sap/cloud/service/flags/demo/service/EvaluationException.java
@@ -2,8 +2,8 @@ package com.sap.cloud.service.flags.demo.service;
 
 public class EvaluationException extends RuntimeException {
 
-  public EvaluationException(String message) {
-    super(message);
-  }
+	public EvaluationException(String message) {
+		super(message);
+	}
 
 }
